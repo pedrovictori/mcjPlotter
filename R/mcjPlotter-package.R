87b33a236a1fccea89f333716896df37c5883fd6
Pedro Victori <fosforilasa@gmail.com>
2022-04-14 15:23:00 +0100

    package setup

---
+++ R/mcjPlotter-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
+NULL
