8daf59c06367cd1cb0abb953edf62c46b14e89df
Pedro Victori <21984609+pedrovictori@users.noreply.github.com>
2022-10-16 14:15:19 +0100

    plotCounts

--- R/commons.R
+++ R/commons.R
@@ -14,3 +14,24 @@ loadAndBind = function(directory){
   data = bind_rows(all, .id = 'filename')
   data
 }
+
+#' Plot standard microCJ data
+#'
+#' @param data microCJ data
+#' @param timesteps how many steps in the data
+#'
+#' @return A ggplot2 object
+#' @export
+plotCounts = function(data, timesteps = 3000){
+  p = data %>%
+    ggplot(aes(x = timepoint, y = mean, colour = pop, fill = pop)) +
+    geom_line() +
+    geom_ribbon(aes(ymin = mean - sterr, ymax = mean + sterr), alpha = 0.2, linetype = 0) +
+    xlim(0, timesteps) +
+    theme_pubr(legend = "top") +
+    labs(
+      y = "Cell count", x = "Time step",
+      colour = "Subpopulation", fill = "Subpopulation"
+    )
+  p
+}
